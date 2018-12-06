1b10ff57535913caff4f7d959556f6063eb0d8fd
anee12 <45456525+anee12@users.noreply.github.com>
2018-12-06 18:38:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var account =
+    {
+    accounttittle: "aneeqa ilyas",
+    accountblance: 100000,
+    accountcurrency: "PKRS",
+    IBAN_NO: "123456789",
+}
+ display:funcation (){
+
+    document.getElementById("title").innerHTML =account.AccountTitle;
+    document.getElementById("balance").innerHTML =account.AccountbBalance;
+    document.getElementById("currency").innerHTML =account.Currenc;
+    document.getElementById("IBAN").innerHTML =account.Account.IBAN;
+    console.log(2);
+
+    deposite:funcation(x){
+        var deposit+document.getElementById("deposit");
+        var balanced=document.getElemntById("blance");
+        balance=deposite+blance;
+
+    }
+withdraw:funcation(y){
+        var withdraw=document.getElemntByid("withdraw");
+
+    }
+}
+account.display();
